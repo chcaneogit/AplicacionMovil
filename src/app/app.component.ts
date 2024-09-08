574ea802472e940af0416003f09e7636715c9115
smipt <marcospachecosaez@gmail.com>
2024-09-08 11:03:26 -0300

    cambios en barra menú
    ahora se oculta la barra cuando apretamos en los botones de menú

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { AutentificacionService } from './service/autentificacion/autentificacion.service';
+import { MenuController } from '@ionic/angular';  // Importa el controlador del menú
 import { UsuariosService } from './service/usuarios/usuarios.service';
 import { Usuario } from './models/usuario';
 
@@ -10,12 +11,27 @@ import { Usuario } from './models/usuario';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  menuDisabled: boolean = false; // Nueva variable para controlar si el menu esta deshabilitado
 
   constructor(
     private _authService: AutentificacionService,
     private router: Router,
+    private menuCtrl: MenuController,
     private _usuarioService: UsuariosService
-  ) {}
+  ) {
+     // Suscríbete al evento de cambio de navegación
+     this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Define las rutas donde deseas cerrar el menú
+        const rutasSinMenu = ['/login', '/home', '/dashboard'];
+        if (rutasSinMenu.includes(this.router.url)) {
+          this.menuCtrl.close();  // Cierra el menú en estas rutas
+        } else {
+          this.menuCtrl.open();   // Abre el menú en las demás rutas
+        }
+      }
+    });
+  }
 
   ngOnInit() {
     // Verifica el estado de autenticación al iniciar
