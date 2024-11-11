5b50564472992bc04c833ad1919bb308a06f977c
Christian C <caneovilches46@gmail.com>
2024-11-10 23:12:15 -0300

    Incorporacion de GPS

--- src/app/page/reportes/reportes.page.ts
+++ src/app/page/reportes/reportes.page.ts
@@ -5,6 +5,9 @@ import { AlertController } from '@ionic/angular';
 import { AutenticacionService } from '../../service/autenticacion/autenticacion.service';
 import { Router } from '@angular/router';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Geolocation } from '@capacitor/geolocation';
+
+declare var google: any;
 
 @Component({
   selector: 'app-reportes',
@@ -13,6 +16,8 @@ import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 })
 export class ReportesPage implements OnInit {
   reportes: any[] = [];
+  map: any;
+  userPosition: { lat: number; lng: number } | null = null;
 
   nuevoReporte = {
     id_region: '',
@@ -24,7 +29,7 @@ export class ReportesPage implements OnInit {
     fecha_publicacion: '',
     desconocidoModelo: false,
     desconocidoPatente: false,
-    foto: '',  // Guardará la URI de la foto tomada
+    foto: '',
   };
 
   constructor(
@@ -36,6 +41,50 @@ export class ReportesPage implements OnInit {
 
   ngOnInit() {
     this.cargarReportes();
+    this.loadGoogleMaps();
+  }
+
+  async loadGoogleMaps() {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDRsFGmvUnCW63BCMGfwpCfqBIswI_KWUE`; // Reemplaza con tu clave de API de Google Maps
+    script.async = true;
+    script.defer = true;
+    script.onload = async () => {
+      await this.getUserLocation(); // Obtén la ubicación del usuario
+      this.initializeMap();
+    };
+    document.body.appendChild(script);
+  }
+
+  async getUserLocation() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      this.userPosition = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      console.log("Ubicación del usuario:", this.userPosition);
+    } catch (error) {
+      console.error("Error al obtener la ubicación del usuario:", error);
+      this.userPosition = { lat: -34.397, lng: 150.644 }; // Coordenadas predeterminadas en caso de error
+    }
+  }
+
+  initializeMap() {
+    const mapOptions = {
+      center: this.userPosition || { lat: -34.397, lng: 150.644 }, // Ubicación actual o coordenadas por defecto
+      zoom: 15, // Nivel de zoom cercano para la ubicación actual
+    };
+    this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    // Agregar marcador en la ubicación actual del usuario
+    if (this.userPosition) {
+      new google.maps.Marker({
+        position: this.userPosition,
+        map: this.map,
+        title: "Ubicación Actual",
+      });
+    }
   }
 
   cargarReportes() {
@@ -50,49 +99,17 @@ export class ReportesPage implements OnInit {
   }
 
   onDesconocidoModelo() {
-    if (this.nuevoReporte.desconocidoModelo) {
-      this.nuevoReporte.modelo = 'Desconocido';
-    } else {
-      this.nuevoReporte.modelo = '';
-    }
+    this.nuevoReporte.modelo = this.nuevoReporte.desconocidoModelo ? 'Desconocido' : '';
   }
 
   onDesconocidoPatente() {
-    if (this.nuevoReporte.desconocidoPatente) {
-      this.nuevoReporte.patente = 'Desconocido';
-    } else {
-      this.nuevoReporte.patente = '';
-    }
+    this.nuevoReporte.patente = this.nuevoReporte.desconocidoPatente ? 'Desconocido' : '';
   }
 
   async agregarReporte() {
     const today = new Date();
     this.nuevoReporte.fecha_publicacion = today.toISOString().split('T')[0];
-    console.log("Formulario enviado");
-
-    // Validaciones previas
-    if (!this.nuevoReporte.id_region || !this.nuevoReporte.id_tipo_vehiculo || !this.nuevoReporte.id_marca ||
-      (!this.nuevoReporte.modelo && !this.nuevoReporte.desconocidoModelo) ||
-      (!this.nuevoReporte.patente && !this.nuevoReporte.desconocidoPatente) ||
-      !this.nuevoReporte.color) {
-      await this.presentErrorAlert('Por favor, completa todos los campos requeridos.');
-      return;
-    }
-
-    // Validación de la patente
-    if (this.nuevoReporte.patente && !this.nuevoReporte.desconocidoPatente) {
-      const patenteLength = this.nuevoReporte.patente.length;
-      if (patenteLength < 5 || patenteLength > 6) {
-        await this.presentErrorAlert('Patente no corresponde');
-        return;
-      }
-    }
-
-    // Validación de foto: Si no hay foto, no permitir enviar el reporte
-    if (!this.nuevoReporte.foto) {
-      await this.presentErrorAlert('Por favor, toma una foto antes de enviar el reporte.');
-      return;
-    }
+    if (!this.validarFormulario()) return;
 
     this.autenticacionService.obtenerRutUsuario().subscribe({
       next: (rutUsuario) => {
@@ -102,45 +119,53 @@ export class ReportesPage implements OnInit {
         }
 
         const reporteData = {
-          id_region: this.nuevoReporte.id_region,
-          id_tipo_vehiculo: this.nuevoReporte.id_tipo_vehiculo,
-          color: this.nuevoReporte.color,
-          patente: this.nuevoReporte.desconocidoPatente ? 'Desconocido' : this.nuevoReporte.patente,
-          modelo: this.nuevoReporte.desconocidoModelo ? 'Desconocido' : this.nuevoReporte.modelo,
-          id_marca: this.nuevoReporte.id_marca,
-          fecha_publicacion: this.nuevoReporte.fecha_publicacion,
+          ...this.nuevoReporte,
           rut_usuario: rutUsuario,
-          foto: '' // Se dejará vacío aquí, se llenará más tarde
+          foto: '',
         };
 
-        // Subir la foto si se ha capturado una
         if (this.nuevoReporte.foto) {
           this.subirFoto(this.nuevoReporte.foto).then((publicUrl) => {
-            reporteData.foto = publicUrl; // Incluir la URL de la foto subida
+            reporteData.foto = publicUrl;
 
-            // Agregar el reporte con la URL de la foto
             this.supabaseService.addReporte(reporteData).subscribe({
-              next: async (response) => {
-                console.log('Respuesta del servidor:', response);
+              next: async () => {
                 this.cargarReportes();
                 this.resetNuevoReporte();
                 await this.presentSuccessAlert('Reporte agregado con éxito.');
               },
-              error: (err) => {
-                console.error('Error al agregar reporte:', err);
+              error: () => {
                 this.presentErrorAlert('Error al agregar el reporte. Intenta nuevamente.');
               }
             });
           });
         }
       },
-      error: (err) => {
-        console.error('Error al obtener el RUT del usuario:', err);
+      error: () => {
         this.presentErrorAlert('Error al obtener el RUT del usuario.');
       }
     });
+  }
 
-    console.log('Reporte agregado:', this.nuevoReporte);
+  validarFormulario(): boolean {
+    const { id_region, id_tipo_vehiculo, id_marca, modelo, patente, color, foto, desconocidoModelo, desconocidoPatente } = this.nuevoReporte;
+
+    if (!id_region || !id_tipo_vehiculo || !id_marca || (!modelo && !desconocidoModelo) || (!patente && !desconocidoPatente) || !color) {
+      this.presentErrorAlert('Por favor, completa todos los campos requeridos.');
+      return false;
+    }
+
+    if (patente && !desconocidoPatente && (patente.length < 5 || patente.length > 6)) {
+      this.presentErrorAlert('Patente no corresponde');
+      return false;
+    }
+
+    if (!foto) {
+      this.presentErrorAlert('Por favor, toma una foto antes de enviar el reporte.');
+      return false;
+    }
+
+    return true;
   }
 
   async presentErrorAlert(message: string) {
@@ -149,7 +174,6 @@ export class ReportesPage implements OnInit {
       message: message,
       buttons: ['OK'],
     });
-
     await alert.present();
   }
 
@@ -159,7 +183,6 @@ export class ReportesPage implements OnInit {
       message: message,
       buttons: ['OK'],
     });
-
     await alert.present();
   }
 
@@ -178,7 +201,7 @@ export class ReportesPage implements OnInit {
       fecha_publicacion: '',
       desconocidoModelo: false,
       desconocidoPatente: false,
-      foto: '' // Reiniciar la foto
+      foto: '',
     };
   }
 
@@ -187,13 +210,10 @@ export class ReportesPage implements OnInit {
       const image = await Camera.getPhoto({
         quality: 90,
         allowEditing: false,
-        resultType: CameraResultType.Uri,  // Usar Uri para obtener una referencia de la foto
-        source: CameraSource.Camera
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera,
       });
-
-      // Guardar la URI de la imagen capturada
       this.nuevoReporte.foto = image.webPath!;
-      console.log('Foto tomada correctamente:', this.nuevoReporte.foto);
     } catch (error) {
       console.error('Error al tomar la foto:', error);
     }
@@ -203,17 +223,9 @@ export class ReportesPage implements OnInit {
     try {
       const response = await fetch(fotoUri);
       const blob = await response.blob();
-
-      // Generar un nombre único para la imagen en el bucket (por ejemplo, con un timestamp)
-      const fileName = `${new Date().getTime()}.jpg`; // Nombre único basado en el timestamp
-
-      // Subir la imagen al bucket de Supabase
+      const fileName = `${new Date().getTime()}.jpg`;
       const filePath = await this.supabaseService.uploadFile(fileName, blob);
-
-      // Obtener la URL pública de la imagen
-      const publicUrl = this.supabaseService.getPublicUrl(filePath);
-      console.log('Foto subida correctamente:', publicUrl);
-      return publicUrl;
+      return this.supabaseService.getPublicUrl(filePath);
     } catch (error) {
       console.error('Error al subir la foto:', error);
       throw error;
